a1c801207405b5d23007a4876d12e4ae7826b9b7
MylesTrump <>
2021-06-01 20:29:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Created by: Myles Trump
+# Created on: May 2021
+# This program formats your address
+
+
+def rounding(rounding_num, decimal):
+
+    # process
+    integer = decimal * 10 ** rounding_num
+    decimal_rounded = int(integer)
+    decimal_final = decimal_rounded / 10 ** rounding_num
+
+    # output
+    print("\nYour number is rounded to {0}".format(decimal_final))
+
+
+def main():
+    # this function calls other functions as well as
+    #   takes input
+
+    # input
+    decimal = float(input("Input a decimal to round: "))
+    print("Input how many decimal places ", end="")
+    rounding_num = int(input("you'd like to round to: "))
+
+    # call fucntions
+    rounding(rounding_num, decimal)
+
+
+if __name__ == "__main__":
+    main()
